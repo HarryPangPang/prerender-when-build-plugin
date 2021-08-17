82315177c429e5234b57a29f13d5f5c06145a212
harry <harryzhang@lilith.com>
2021-08-17 17:00:40 +0800

    fix: responese utf8

--- puppeteer.js
+++ puppeteer.js
@@ -34,21 +34,36 @@ class PuppeteerRenderer {
 	async handleRequestInterception(page, baseURL) {
 		await page.setRequestInterception(true)
 		page.on('request', req => {
-			// 跳过请求
-			if (this._options.skipRequest&&['fetch', 'xhr', 'websocket'].indexOf(req.resourceType()) > -1) {
-				req.abort()
-				return
+			// 跳过api请求
+			const isLocalApi = ['fetch', 'xhr', 'websocket'].indexOf(req.resourceType()) > -1 && req.url().startsWith(baseURL)
+			// 跳过所有请求
+			if (this._options.skipRequest && isLocalApi) {
+				req.abort();
+				return;
 			}
-			let apiPath = req._url.split('/')
-			apiPath = '/' + apiPath.splice(3).join('/')
-			if(this._options.mock&&Object.prototype.toString.call(this._options.mock)=== "[object Object]"&&this._options.mock[apiPath]){
+
+			if (this._options.forceMock && Object.prototype.toString.call(this._options.forceMock) === '[object Object]' && this._options.forceMock[req.url()]) {
 				req.respond({
-					content: 'application/json',
-					body: JSON.stringify(this._options.mock[apiPath])
-				  })
-				  return
+					contentType: 'application/json; charset=utf-8',
+					body: JSON.stringify(this._options.forceMock[req.url()]),
+				});
+				return;
 			}
-			
+			if (isLocalApi) {
+				// mock地址
+				let apiPath = req.url().split('/');
+				apiPath = '/' + apiPath.splice(3).join('/');
+				if (
+					this._options.mock &&
+					Object.prototype.toString.call(this._options.mock) === '[object Object]' && this._options.mock[apiPath]
+				) {
+					req.respond({
+						contentType: 'application/json; charset=utf-8',
+						body: JSON.stringify(this._options.mock[apiPath])
+					});
+					return;
+				}
+			}
 			req.continue()
 		})
 	}
@@ -59,18 +74,18 @@ class PuppeteerRenderer {
 	}
 
 	// 渲染指定路径
-	async renderRoutes (routes, Prerenderer) {
+	async renderRoutes(routes, Prerenderer) {
 		const options = Prerenderer.getOptions()
 		const pagePromises = Promise.all(
-			routes.map(async route=>{
+			routes.map(async route => {
 				const page = await this._puppeteer.newPage()
 				const baseURL = `http://localhost:${options.port}`
-				await page.setViewport(options.renderOption ? (options.renderOption.viewport || {'width':1920,'height':1080}) : {'width':1920,'height':1080})
+				await page.setViewport(options.renderOption ? (options.renderOption.viewport || { 'width': 1920, 'height': 1080 }) : { 'width': 1920, 'height': 1080 })
 				await this.handleRequestInterception(page, baseURL)
 				await page.goto(`${baseURL}${route}`, { waituntil: 'networkidle2', domcontentloaded: true, timeout: 60000000 });
 				// 等待某个class
-				if (options.renderOption&&options.renderOption.waitForElement && typeof options.renderOption.waitForElement === 'string') {
-				  await page.waitForSelector(options.renderOption.waitForElement)
+				if (options.renderOption && options.renderOption.waitForElement && typeof options.renderOption.waitForElement === 'string') {
+					await page.waitForSelector(options.renderOption.waitForElement)
 				}
 				// sleep 1s
 				await page.waitForTimeout(1000)
@@ -84,7 +99,7 @@ class PuppeteerRenderer {
 				return result
 			})
 		)
-	
+
 		return pagePromises
 	}
 }
